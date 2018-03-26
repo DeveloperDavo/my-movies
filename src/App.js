3fe3a1f24e211501b578a119144496f7f4e42d51
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-03-26 14:17:12 +0200

    Add MinRatingSelect and set minRating to 4.5

--- src/App.js
+++ src/App.js
@@ -21,6 +21,23 @@ const SearchBar = ({ value, onChange }) => {
   )
 }
 
+const MinRatingSelect = ({ value, onChange }) => {
+  const options = [<option value="none">none</option>];
+  for (let i = 3; i <= 5; i = i + 0.5) {
+    options.push(
+      <option value={i}>{i}</option>
+    )
+  }
+  return (
+    <div>
+      <label>Choose a minimum rating: </label>
+      <select value={value} onChange={onChange}>
+        {options}
+      </select> 
+    </div>
+  )
+}
+
 const RestaurantGrid = () => {
   const restaurantGrid = businesses.map(business => {
     return (
@@ -40,18 +57,24 @@ const RestaurantGrid = () => {
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {searchText: ''};
+    this.state = {searchText: '', minRating: "4.5"};
     this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+    this.handleMinRatingChange = this.handleMinRatingChange.bind(this);
   }
 
   handleSearchTextChange(e) {
     this.setState({searchText: e.target.value})
   }
 
+  handleMinRatingChange(e) {
+    this.setState({minRating: e.target.value})
+  }
+
   render() {
     return (
       <main>
         <SearchBar value={this.state.searchText} onChange={this.handleSearchTextChange} />
+        <MinRatingSelect value={this.state.minRating} onChange={this.handleMinRatingChange} />
         <RestaurantGrid />
       </main>
     )
