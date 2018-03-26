78ca18df22f9b6ed5dfa839cb4eaa4925066028f
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-03-26 10:02:25 +0200

    Show list of static business names

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { businesses } from './businesses.json';
 
 class App extends Component {
   render() {
+    const businessNames = businesses.map(business => {
+      return (
+        <p key={business.id}>
+          {business.name}
+        </p>
+      );
+    })
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {businessNames}
       </div>
     );
   }
