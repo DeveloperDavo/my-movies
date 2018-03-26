6bba2aa8ceea222ebbac6e002e3ca948295ade6a
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-03-26 13:17:20 +0200

    Extract Header and Main

--- src/App.js
+++ src/App.js
@@ -3,29 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 import { businesses } from './businesses.json';
 
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Restaurant Finder</h1>
+    </header>
+  )
+}
+
+const Main = () => {
+  const mainContent = businesses.map(business => {
+    return (
+      <div key={business.id}>
+        <a href={business.url}>
+          <img width="200" height="200" src={business.image_url} alt={business.name} />
+          <div>{business.name}</div>
+        </a>
+        <div>rating: {business.rating}</div>
+        <div>reviews: {business.review_count}</div>
+      </div>
+    );
+  })
+  return (
+    <main>
+      {mainContent}
+    </main>
+  )
+}
+
 class App extends Component {
   render() {
-    const mainContent = businesses.map(business => {
-      return (
-        <div key={business.id}>
-          <a href={business.url}>
-            <img width="200" height="200" src={business.image_url} alt={business.name} />
-            <div>{business.name}</div>
-          </a>
-          <div>rating: {business.rating}</div>
-          <div>reviews: {business.review_count}</div>
-        </div>
-      );
-    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Restaurant Finder</h1>
-        </header>
-        <main>
-          {mainContent}
-        </main>
+        <Header />
+        <Main />
       </div>
     );
   }
