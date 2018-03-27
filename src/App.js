57ca886525cf985b8aa2ee2c41f827f66b36d63a
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-03-27 08:35:12 +0200

    Retrieve and log movies using Fetch API

--- src/App.js
+++ src/App.js
@@ -78,6 +78,16 @@ class Main extends Component {
 }
 
 class App extends Component {
+  componentDidMount() {
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+    });
+  }
+
   render() {
     return (
       <div className="App">
