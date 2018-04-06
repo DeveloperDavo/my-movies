6893369b14fda9cda68aed5f849799d2be9a6bc6
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-06 18:01:28 +0200

    Move exports from App to MainComponent

--- src/App.js
+++ src/App.js
@@ -11,53 +11,6 @@ const Header = () => {
   );
 };
 
-export const MinRatingSelect = ({ value, onChange }) => {
-  const options = [<option key="0" value="0">none</option>];
-  for (let i = 5; i <= 10; i = i + 0.5) {
-    options.push(<option key={i} value={i}>{i}</option>);
-  }
-  return (
-    <div>
-      <label>Choose a minimum rating: </label>
-      <select className="MinRatingSelect--select" value={value} onChange={onChange}>
-        {options}
-      </select>
-    </div>
-  );
-};
-
-export const MovieGrid = ({ movies, minRating }) => {
-  const movieGrid = movies
-    .filter(movie => movie.vote_average >= minRating)
-    .map(movie => {
-      const imageSrc = `http://image.tmdb.org/t/p/w200/${movie.poster_path}`;
-      return (
-        <div className="MovieGrid--movie-item" key={movie.id}>
-          <img
-            width="200"
-            src={imageSrc}
-            alt={movie.title}
-          />
-          <h3>{movie.title}</h3>
-          <div>Rating: {movie.vote_average}</div>
-          <div>Review Count: {movie.vote_count}</div>
-        </div>
-      );
-    });
-  return <div className="MovieGrid">{movieGrid}</div>;
-};
-
-export const Attribution = () => {
-  return (
-    <footer className="Attribution">
-      <img className="Attribution--logo"src="tmdb-logo.png" alt="tmdb logo"/>
-      <div className="Attribution--text">
-        This product uses the TMDb API but is not endorsed or certified by TMDb.
-      </div>
-    </footer>
-  )
-}
-
 class App extends Component {
   constructor(props) {
     super(props);
