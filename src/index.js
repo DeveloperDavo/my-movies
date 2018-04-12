2f9a65785f63d9c3b187842e90baf959dfe1fdcd
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-12 17:32:26 +0200

    Extract store from index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import rootReducer from './reducers';
+import { store } from './store'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware
-  )
-);
 
 ReactDOM.render(
   <Provider store={store}>
