70b3f7a4653c1bdf7bd5772296334eb328c9fc6b
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-12 20:14:04 +0200

    Mock containers in App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { store } from './store'
 import App from './App';
+
+jest.mock('./containers');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const provider = 
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ReactDOM.render(provider, div);
   ReactDOM.unmountComponentAtNode(div);
 });
