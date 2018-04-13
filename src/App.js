67ae7f79a2387245e58bec01796edf497221e45b
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-13 18:00:58 +0200

    Replace App.css with AppWrapper in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import "./App.css";
 import { Main } from "./containers";
 import { Header } from "./components";
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
+    const AppWrapper = styled.div`
+      text-align: center;
+    `;
     return (
-      <div className="App">
+      <AppWrapper>
         <Header />
         <Main />
-      </div>
+      </AppWrapper>
     );
   }
 }
