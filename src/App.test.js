0223f182ce3a115ccdf6355a7e800b21b90fb619
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-13 18:52:09 +0200

    Use shallow rendering in App.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App';
-
-jest.mock('./containers');
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const provider = 
-    <Provider store={store}>
-      <App />
-    </Provider>
-  ReactDOM.render(provider, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
