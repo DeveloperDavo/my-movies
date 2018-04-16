b7d5de29f11fd2701ef3e057f29d2444e941ed6f
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-16 11:02:48 +0200

    Add test for handling changing the min rating

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { handleUserActions } from "./reducers"
+import { changeMinRating } from "./actions";
 
 describe("reducers", () => {
   it("should return handle initial state", () => {
     expect(handleUserActions(undefined, {})).toEqual({
       minRating: 0
+    })
+  });
+
+  it("should return handle changing the min rating", () => {
+    const minRating = 7;
+    expect(handleUserActions(undefined, changeMinRating(minRating))).toEqual({
+      minRating: 7
     })
   });
 });
