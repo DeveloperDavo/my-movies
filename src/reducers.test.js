d02c87f080f8a281c42dac6c18735179432d013a
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-16 21:05:32 +0200

    Fix typos in reducers.test.js

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -3,13 +3,13 @@ import { handleUserActions } from "./reducers"
 import { changeMinRating } from "./actions";
 
 describe("reducers", () => {
-  it("should return handle initial state", () => {
+  it("should return initial state", () => {
     expect(handleUserActions(undefined, {})).toEqual({
       minRating: 0
     })
   });
 
-  it("should return handle changing the min rating", () => {
+  it("should handle changing the min rating", () => {
     const minRating = 7;
     expect(handleUserActions(undefined, changeMinRating(minRating))).toEqual({
       minRating: 7
