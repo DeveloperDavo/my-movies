6e7148c36ad711c43631e70f32f9a34c9fdc8a7e
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-19 18:06:12 +0200

    Add snapshot for empty MainComponent

--- src/components.test.js
+++ src/components.test.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Header } from "./components";
+import { Header, MainComponent } from "./components";
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import { results } from "./movies.json";
 
 describe("Header", () => {
   it("should match snapshot", () => {
     const tree = renderer.create(
       <Header/>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  })
+})
+
+describe("Main component", () => {
+  it("should match empty snapshot", () => {
+    const mainComp = 
+      <MainComponent 
+        handleMinRatingChange={() => {}} 
+        handleFetchMovies={() => {}} 
+        minRating={0}
+        movies={[]}
+      />
+    const tree = renderer.create(
+      mainComp
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
