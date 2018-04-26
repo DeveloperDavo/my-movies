d87b769856523e2f40e836d183fbcdb22d8d8f99
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-04-26 18:35:42 +0200

    Add test for handling receiving movies in reducers.test.js

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { handleUserActions } from "./reducers"
-import { changeMinRating } from "./actions";
+import { handleUserActions, handleNetworkActions } from "./reducers"
+import { changeMinRating, receiveMovies } from "./actions";
+import { results } from "./movies.json";
 
 describe("reducers", () => {
-  it("should return initial state", () => {
-    expect(handleUserActions(undefined, {})).toEqual({
-      minRating: 0
-    })
+  describe("user actions", () => {
+    it("should return initial state", () => {
+      expect(handleUserActions(undefined, {})).toEqual({
+        minRating: 0
+      })
+    });
+
+    it("should handle changing the min rating", () => {
+      const minRating = 7;
+      expect(handleUserActions(undefined, changeMinRating(minRating))).toEqual({
+        minRating: 7
+      })
+    });
   });
 
-  it("should handle changing the min rating", () => {
-    const minRating = 7;
-    expect(handleUserActions(undefined, changeMinRating(minRating))).toEqual({
-      minRating: 7
-    })
-  });
+  describe("network actions", () => {
+    it("should handle receiving movies", () => {
+      expect(handleNetworkActions({movies: []}, receiveMovies(results))).toEqual({
+        movies: results
+      })
+    });
+  })
 });
