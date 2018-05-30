4a3f8e1b26d5f42faec89d27bd74d6da01c00409
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-05-30 16:29:52 +0200

    Replace 'return' in mapStateToProps in containers.js

--- src/containers.js
+++ src/containers.js
@@ -2,12 +2,10 @@ import { connect } from 'react-redux'
 import { changeMinRating, fetchMovies } from './actions'
 import { MainComponent } from './components'
 
-const mapStateToProps = state => {
-  return {
-    minRating: state.handleUserActions.minRating,
-    movies: state.handleNetworkActions.movies
-  }
-}
+const mapStateToProps = state => ({
+  minRating: state.handleUserActions.minRating,
+  movies: state.handleNetworkActions.movies
+})
 
 const mapDispatchToProps = dispatch => ({
   handleMinRatingChange: e => dispatch(changeMinRating(e.target.value)),
